de1cbc958debf8ceb2eb417b5e897e4f4746df96
Vincent Kibet <128219446+vinkibe@users.noreply.github.com>
2024-05-30 17:03:50 +0300

    Add files via upload

---
+++ frontend/static/js/views/Services.js
@@ -0,0 +1,59 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView{
+    constructor(){
+        super();
+        this.setTitle("Services");
+    }
+
+    async getHtml(){
+       return `
+       <section class="menu" id="menu">
+       <h1 class="heading"> Our <span>menu</span></h1>
+       <div class="box-container">
+         <div class="box">
+           <img src="static/assets/fried.jpg" alt="">
+           <h3>Tasty and Healthy</h3>
+           <div class="price">$15.99 <span>20.99</span></div>
+           <a href="#" class="btn">Add to Cart</a>
+         </div>
+     
+         <div class="box">
+           <img src="static/assets/food-6.jpg" alt="">
+           <h3>Tasty and Healthy</h3>
+           <div class="price">$15.99 <span>20.99</span></div>
+           <a href="#" class="btn">Add to Cart</a>
+         </div>
+     
+         <div class="box">
+           <img src="static/assets/food-5.jpg" alt="">
+           <h3>Tasty and Healthy</h3>
+           <div class="price">$15.99 <span>20.99</span></div>
+           <a href="#" class="btn">Add to Cart</a>
+         </div>
+     
+         <div class="box">
+           <img src="static/assets/food-1.jpg" alt="">
+           <h3>Tasty and Healthy</h3>
+           <div class="price">$15.99 <span>20.99</span></div>
+           <a href="#" class="btn">Add to Cart</a>
+         </div>
+     
+         <div class="box">
+           <img src="static/assets/food-4.jpg" alt="">
+           <h3>Tasty and Healthy</h3>
+           <div class="price">$15.99 <span>20.99</span></div>
+           <a href="#" class="btn">Add to Cart</a>
+         </div>
+     
+         <div class="box">
+           <img src="static/assets/food-3.jpg" alt="">
+           <h3>Tasty and Healthy</h3>
+           <div class="price">$15.99 <span>20.99</span></div>
+           <a href="#" class="btn">Add to Cart</a>
+         </div>
+       </div>
+     </section>
+       `;
+    }
+}
