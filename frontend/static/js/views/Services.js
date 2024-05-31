d1ab047bf71f2f21f3dc028076792d47369fd505
Vincent Kibet <128219446+vinkibe@users.noreply.github.com>
2024-05-31 10:44:53 +0300

    Update Services.js

--- frontend/static/js/views/Services.js
+++ frontend/static/js/views/Services.js
@@ -21,35 +21,35 @@ export default class extends AbstractView{
          <div class="box">
            <img src="static/food-6.jpg" alt="">
            <h3>Tasty and Healthy</h3>
-           <div class="price">$15.99 <span>20.99</span></div>
+           <div class="price">15.99 ksh.<span>20.99</span></div>
            <a href="#" class="btn">Add to Cart</a>
          </div>
      
          <div class="box">
            <img src="static/food-5.jpg" alt="">
            <h3>Tasty and Healthy</h3>
-           <div class="price">$15.99 <span>20.99</span></div>
+           <div class="price">15.99 ksh.<span>20.99</span></div>
            <a href="#" class="btn">Add to Cart</a>
          </div>
      
          <div class="box">
            <img src="static/food-1.jpg" alt="">
            <h3>Tasty and Healthy</h3>
-           <div class="price">$15.99 <span>20.99</span></div>
+           <div class="price">15.99 ksh.<span>20.99</span></div>
            <a href="#" class="btn">Add to Cart</a>
          </div>
      
          <div class="box">
            <img src="static/food-4.jpg" alt="">
            <h3>Tasty and Healthy</h3>
-           <div class="price">$15.99 <span>20.99</span></div>
+           <div class="price">15.99 ksh. <span>20.99</span></div>
            <a href="#" class="btn">Add to Cart</a>
          </div>
      
          <div class="box">
            <img src="static/food-3.jpg" alt="">
            <h3>Tasty and Healthy</h3>
-           <div class="price">$15.99 <span>20.99</span></div>
+           <div class="price">15.99 ksh. <span>20.99</span></div>
            <a href="#" class="btn">Add to Cart</a>
          </div>
        </div>
